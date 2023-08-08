b7e1218e8568dfbe792e5b2715dd8258465ecd9a
Vicky122511 <vicky.shah7928@yahoo.com>
2023-08-08 20:43:29 +0100

    Nevil Homework

---
+++ src/main/java/com/bestbuy/contants/Path.java
@@ -0,0 +1,9 @@
+package com.bestbuy.contants;
+
+public class Path {
+    public static final String PRODUCTS = "/products";
+    public static final String STORES = "/stores";
+    public static final String CATEGORIES = "/categories";
+    public static final String SERVICES = "/services";
+    public static final String UTILITIES = "";
+}
